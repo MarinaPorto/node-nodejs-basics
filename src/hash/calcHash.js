a80b4eccd8a7f6cfd901c7088dff11ae0a5d7f60
Maryna Zhukovich <marina.x.sea@gmail.com>
2022-12-05 21:09:11 +0300

    add: problem solutions (hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(filePath, 'utf-8');
+  readStream.on('data', (chunk) => {
+    const hash = crypto.createHash('sha256').update(chunk).digest('hex');
+    console.log(hash)
+  });
 };
 
 await calculateHash();
