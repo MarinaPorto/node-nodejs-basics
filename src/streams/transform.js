e031e1eb65d64ccd33e555526bcf8b19780215da
Maryna Zhukovich <marina.x.sea@gmail.com>
2022-12-05 20:51:08 +0300

    add: problem solutions (streams)

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { Transform } from 'stream';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const transform = async () => {
-    // Write your code here 
+  const createTransformStream = () => {
+    return new Transform({
+      transform(chunk, enc, cb) {
+        let str = chunk.toString();
+        let reverseStr = [...str].reverse().join("");
+        cb(null, reverseStr);
+      }
+    });
+  };
+
+  const res = createTransformStream();
+  process.stdin.pipe(res).pipe(process.stdout)
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  process.stdin.on('data', function (data) {
+    const writeStream = fs.createWriteStream(filePath, { 'flag': 'a' });
+    writeStream.write(data);
+  })
+
 };
 
 await write();
